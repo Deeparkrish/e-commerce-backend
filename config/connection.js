3de5db8d654cfba3f2c497c46392e0cc86bdcc92
Deepa <kosalai17@gmail.com>
2021-07-26 21:08:30 -0700

    add association

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
-
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
+require('dotenv').config();
+
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
